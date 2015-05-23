6e4fa061c36e1b932c6477d185d8225df3f7f728
Edmondo Pentangelo <epentangelo@Edmondos-MacBook.local>
2015-05-24 00:09:45 +0100

    Initial commit

---
+++ src/InvertedIndex.hs
@@ -0,0 +1,50 @@
+module InvertedIndex where
+
+  import Data.Function
+  import Data.Char
+  import Data.List
+  import Data.Map (Map, fromList, (!))
+  import Control.Applicative
+
+  type Doc = String
+  type Line = String
+  type Word = String
+  type LineNumber = Int
+  type Index = Map Word [LineNumber]
+  type Frequency = Int
+
+  both :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+  both = liftA2 (&&)
+
+  numLines :: Doc -> [(Line, LineNumber)]
+  numLines = flip zip [1..] . lines
+
+  cleanWords :: [Word] -> [Word]
+  cleanWords = filter (not . null) . map (filter (both isLetter isAscii))
+
+  allNumWords :: [(Line, LineNumber)] -> [(Word, LineNumber)]
+  allNumWords = (>>= (\(l, i) -> map (\w -> (w, i)) $ cleanWords $ words l))
+
+  makeLists :: [(Word, LineNumber)] -> [(Word, [LineNumber])]
+  makeLists = map (\((w, i)) -> (w, [i]))
+
+  accumulate :: [(Word, [LineNumber])] -> [(Word, [LineNumber])]
+  accumulate = foldl' f []
+             where f [] x = [x]
+                   f ((w, ls):xs) (w2, [l]) | w == w2 = (w, l:ls):xs
+                   f ((w, ls):xs) (w2, [l]) | w /= w2 = (w2, [l]):((w, ls):xs)
+                   f _ _ = fail "What the hell happened"
+
+  createIndex :: Doc -> Index
+  createIndex =  fromList . accumulate . makeLists . sort . allNumWords . numLines
+
+  frequencies :: (Eq a, Ord a) => [a] -> [(a, Frequency)]
+  frequencies = sortBy (flip compare `on` snd) . foldl' count [] . sort
+    where
+      count [] x = [(x, 1)]
+      count ((y, i):xs) x | x == y = (y, i + 1):xs
+      count ((y, i):xs) x | x /= y = (x, 1):((y, i):xs)
+      count _ _ = fail "This should never happen"
+
+  search :: Index -> String -> [(LineNumber, Frequency)]
+  search i = frequencies . concatMap (\w -> i!w) . cleanWords . words
---
+++ tests/Tests.hs
@@ -0,0 +1,102 @@
+module Main where
+
+import InvertedIndex
+import Test.Hspec
+import Data.Map (toList)
+
+main :: IO ()
+main = hspec $ do
+
+  describe "numLines tests" $ do
+
+    it "should return first line with index" $ do
+      numLines "Hello World" `shouldBe` [("Hello World", 1)]
+
+    it "should return two lines with index" $ do
+      numLines "Hello\nWorld" `shouldBe` [("Hello", 1), ("World", 2)]
+
+    it "should return n lines with index" $ do
+      numLines "Welcome to\nReal World\nof Haskell"
+       `shouldBe` [("Welcome to", 1), ("Real World", 2), ("of Haskell", 3)]
+
+  describe "cleanWords tests" $ do
+
+    it "should return words" $ do
+      cleanWords ["Hello", "World"] `shouldBe` ["Hello", "World"]
+
+    it "should remove numbers" $ do
+      cleanWords ["Welcome6"] `shouldBe` ["Welcome"]
+
+    it "should remove non ascii chars" $ do
+      cleanWords ["This,", "is", "an", "experiment'", "of", "great", "%"]
+        `shouldBe` ["This", "is", "an", "experiment", "of", "great"]
+
+  describe "allNumWords tests" $ do
+
+    it "should convert lines in to words keeping line numbers" $ do
+      allNumWords [("Welcome to", 1), ("the real", 2)]
+        `shouldBe` [("Welcome", 1), ("to", 1), ("the", 2), ("real", 2)]
+
+    it "should clean while splitting into words" $ do
+      allNumWords [("Lo ha detto papa'", 1), ("che va' bene, capito", 2)]
+        `shouldBe` [("Lo", 1), ("ha", 1), ("detto", 1), ("papa", 1),
+                    ("che", 2), ("va", 2), ("bene", 2), ("capito", 2)]
+
+  describe "createIndex tests" $ do
+
+    it "should create an empty index for an empty document" $ do
+      (toList . createIndex) ""
+        `shouldBe` []
+
+    it "should create an index of one element for a single word doc" $ do
+      (toList . createIndex) "Hello"
+        `shouldBe` [("Hello", [1])]
+
+    it "should create an index" $ do
+      (toList . createIndex) "Hello World"
+        `shouldBe` [("Hello", [1]), ("World", [1])]
+
+      (toList . createIndex) "Back to the Future\nA few good man"
+        `shouldBe`[("A",[2]),("Back",[1]),("Future",[1]),("few",[2]),
+                   ("good",[2]),("man",[2]),("the",[1]),("to",[1])]
+
+      (toList . createIndex) "A few good men\nAll the President's men"
+        `shouldBe`[("A",[1]),("All",[2]),("Presidents",[2]),("few",[1]),
+                   ("good",[1]),("men",[2,1]),("the",[2])]
+
+      (toList . createIndex) "Back to the future\nIn to the future\nBack in time"
+        `shouldBe`[("Back",[3,1]),("In",[2]),("future",[2,1]),
+                   ("in",[3]),("the",[2,1]),("time",[3]),("to",[2,1])]
+
+  describe "frequencies tests" $ do
+
+    it "should return frequency of one for one item" $ do
+      frequencies ['x'] `shouldBe` [('x', 1)]
+
+    it "should return frequencies of more item" $ do
+      frequencies ['x','y','z'] `shouldBe` [('z', 1), ('y', 1), ('x', 1)]
+      frequencies ['x','y','x'] `shouldBe` [('x', 2), ('y', 1)]
+      frequencies ['x','y','y','x','y']
+        `shouldBe` [('y', 3), ('x', 2)]
+
+  describe "search tests" $ do
+
+     it "should find single word document" $ do
+       let index = createIndex "Hello"
+       search index "Hello" `shouldBe` [(1,1)]
+
+     it "should find word in two words document" $ do
+       let index = createIndex "Hello World"
+       search index "World" `shouldBe` [(1,1)]
+
+     it "should find word in two lines doc" $ do
+       let index = createIndex "Hello\nWorld"
+       search index "World" `shouldBe` [(2, 1)]
+
+     it "should find most likely line" $ do
+       let index = createIndex (
+              "Back to the future\n" ++
+              "Behind enemy lines\n" ++
+              "Behind the planet of the apes\n" ++
+              "Planet of the apes")
+       search index "Behind apes" `shouldBe` [(3,2),(4,1),(2,1)]
